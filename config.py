328fb12ca192100882f1fafe4136249c04691aae
Carol ChanYX <carolchenyx@gmail.com>
2021-02-01 12:35:48 +0800

    update

--- config.py
+++ config.py
@@ -1,27 +1,17 @@
 #Img2json
 
-# image_dir = 'img/yoga_3'
-# json_dir = 'img/yoga_3'
-image_dir = '/media/hkuit155/8221f964-4062-4f55-a2f3-78a6632f7418/Autoannotation_Pose/Img2json/img/yoga_eval'
-# image_dir = "/media/hkuit155/8221f964-4062-4f55-a2f3-78a6632f7418/Autoannotation_Pose/Img2json/img/tree_new"
-json_dir = '/media/hkuit155/8221f964-4062-4f55-a2f3-78a6632f7418/Autoannotation_Pose/Img2json/img/yoga_eval'
-
-
+image_dir = 'img/yoga_3'
+json_dir = 'img/yoga_3'
 yolo_cfg = "yolo/cfg/yolov3-spp.cfg"
 yolo_weight = 'models/yolo/yolov3-spp.weights'
-# yolo_cfg = '../../MODELS/ceiling_detection/0825/yolov3-spp-1cls-leaky.cfg'
-# yolo_weight = "../../MODELS/ceiling_detection/0825/backup40.weights"
-# yolo_cfg = '../../MODELS/underwater_rgb/0902_spp/yolov3-spp-1cls-leaky.cfg'
-# yolo_weight = "../../MODELS/underwater_rgb/0902_spp/best.weights"
-model_path = 'models/sppe/duc_se.pth'
-# model_path = '../../PoseTrainingPytorch/exp/ceiling_tmp/1/1_best.pkl'
-# model_path = '../../MODELS/underwater_pose/underwater_5/underwater_5_best.pkl'
+model_path = '../../PoseTrainingPytorch/exp/ceiling_tmp/1/1_best.pkl'
+
 
 
 #json2h5
 #0_rename_img
-img_folder = "/home/hkuit155/Documents/yoga_train_new1"
-dest_folder = "/home/hkuit155/Documents/yoga_train"
+img_folder = "/home/hkuit155/Documents/yo
+dest_folder = "/home/hkuit155/Documents/yo"
 
 #1_renamejson
 #ceiling pose:3
@@ -30,9 +20,9 @@ dest_folder = "/home/hkuit155/Documents/yoga_train"
 imageandjson_first_number = "1"
 
 number_json_file = 2
-json_filename = '/media/hkuit155/8221f964-4062-4f55-a2f3-78a6632f7418/Autoannotation_Pose/json2h5/json/0612_underwater_all.json'
-output_jsonname = '/media/hkuit155/8221f964-4062-4f55-a2f3-78a6632f7418/Autoannotation_Pose/json2h5/json/0612_underwater_all_rename.json'
+json_filename = '/media/hkuit155/8221f964-4062-4f55-a2f3-78a6632f7418/Autoannotation_Pose/json2h5/json/r_all.json'
+output_jsonname = '/media/hkuit155/8221f964-4062-4f55-a2f3-78a6632f7418/Autoannotation_Pose/json2h5/json/_all_rename.json'
 
 #2_json2h5_others/2_json2h5_kp2json
-input_json = ["/home/hkuit155/Documents/yoga_train.json"]
-output_h5name = '/home/hkuit155/Documents/yoga_train.h5'
+input_json = ["/home/hkuit155/Documents/yo_train.json"]
+output_h5name = '/home/hkuit155/Documents/yo_train.h5'
